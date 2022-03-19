d4181638e825f9bdafd8b1c10c999183abc0fe35
JordanTseng <jordan.ttc.design@gmail.com>
2022-03-19 18:53:19 +0800

    建立環境

---
+++ errorHandler.js
@@ -0,0 +1,18 @@
+function error(res) {
+  const headers = {
+    'Access-Control-Allow-Headers':
+      'Content-Type ,Authorization,Content-Length,X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH,POST,GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
+  };
+  res.writeHead(400, headers);
+  res.write(
+    JSON.stringify({
+      status: 'false',
+      message: '欄位錯誤或id不對',
+    }),
+  );
+  res.end();
+}
+module.exports = error;
